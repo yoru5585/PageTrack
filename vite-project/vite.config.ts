822b199c9322bf6d3187e76f8d89b630e8282767
yoru <96826783+yoru5585@users.noreply.github.com>
2025-05-25 00:40:41 +0900

    add: buildしてみた

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_PAGES  // この行を追加
+    ? "reactailwind"            // この行を追加
+    : "./",
   plugins: [react()],
 })
